f5c00bb027ea0f7ce43d05b7aba81799d63d940c
Yrol <yrollgayanth@yahoo.com>
2021-11-04 22:32:59 +1100

    Changing the title header

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    handleActive : function(component, event, helper) {
+        var tab = event.getSource();
+        var healthHeader = component.find("healthHeader");
 
-    }
+        //changing the CTHealthHeader (child) title on tab change
+        switch (tab.get('v.id')) {
+            case 'location_view' :
+                healthHeader.set("v.title", "Location View");
+                break;
+            case 'person_view' :
+                healthHeader.set("v.title", "Person View");
+                break;
+        }
+    },
 })
